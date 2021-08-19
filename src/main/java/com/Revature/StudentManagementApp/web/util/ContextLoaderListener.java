1ca7fd2d926de228158a23855b4a51aba3fabe7c
Jose Tejada <65978779+tejada3@users.noreply.github.com>
2021-08-18 20:48:45 -0400

    Added faculty, student, course dto

--- src/main/java/com/Revature/StudentManagementApp/web/util/ContextLoaderListener.java
+++ src/main/java/com/Revature/StudentManagementApp/web/util/ContextLoaderListener.java
@@ -1,9 +1,18 @@
 package com.Revature.StudentManagementApp.web.util;
 
+import com.Revature.StudentManagementApp.dataSource.repos.CourseRepo;
+import com.Revature.StudentManagementApp.dataSource.repos.FacultyRepo;
+import com.Revature.StudentManagementApp.dataSource.repos.RegistrationRepo;
 import com.Revature.StudentManagementApp.dataSource.repos.StudentRepo;
+import com.Revature.StudentManagementApp.services.CourseService;
+import com.Revature.StudentManagementApp.services.FacultyService;
+import com.Revature.StudentManagementApp.services.RegistrationService;
 import com.Revature.StudentManagementApp.services.StudentService;
 import com.Revature.StudentManagementApp.util.MongoConnection;
+import com.Revature.StudentManagementApp.web.servlets.CourseServlet;
+import com.Revature.StudentManagementApp.web.servlets.FacultySevlet;
 import com.Revature.StudentManagementApp.web.servlets.HealthCheckServlet;
+import com.Revature.StudentManagementApp.web.servlets.StudentServlet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoClient;
 
@@ -18,13 +27,37 @@ public class ContextLoaderListener implements ServletContextListener {
         MongoClient client = MongoConnection.getInstance().getConnection();
         ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
 
+        ///Studeent Servlet Set up
         StudentRepo studentRepo = new StudentRepo();
         StudentService studentService = new StudentService(studentRepo);
+        StudentServlet studentServlet = new StudentServlet(studentService, mapper);
+
+
+        //Faculty Servlet set up
+        FacultyRepo facultyRepo = new FacultyRepo();
+        FacultyService facultyService = new FacultyService(facultyRepo);
+        FacultySevlet facultyServlet = new FacultySevlet(facultyService, mapper);
+
+        //Course Servlet set up
+
+        RegistrationRepo registrationRepo = new RegistrationRepo();
+        CourseRepo courseRepo = new CourseRepo();
+        RegistrationService registrationService = new RegistrationService(registrationRepo);
+        CourseService courseService = new CourseService(courseRepo, registrationService);
+        CourseServlet courseServlet = new CourseServlet(courseService, mapper);
+
+
 
         HealthCheckServlet healthCheckServlet = new HealthCheckServlet();
 
         ServletContext context = sce.getServletContext();
         context.addServlet("HealthCheckServlet", healthCheckServlet).addMapping("/test");
+        context.addServlet("Studentservlet", studentServlet).addMapping("/students");
+        context.addServlet("Facultyservlet", facultyServlet).addMapping("/faculty");
+        context.addServlet("Courseservlet", courseServlet).addMapping("/course");
+
+
+
     }
 
     @Override
