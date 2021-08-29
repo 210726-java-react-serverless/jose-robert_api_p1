7cdeb4c50ed8be5ce01a5bc73760db4dc27284dd
Robert Ni <robertni@outlook.fr>
2021-08-29 17:22:56 -0400

    students can now register for courses on the start date

--- src/main/java/com/revature/studentmanagement/util/isWithinRegistrationDate.java
+++ src/main/java/com/revature/studentmanagement/util/isWithinRegistrationDate.java
@@ -28,6 +28,6 @@ public class isWithinRegistrationDate {
         Date End = new Date(Integer.parseInt(dateEnd[0]), Integer.parseInt(dateEnd[1])-1, Integer.parseInt(dateEnd[2]));
         Date Today = new Date(Integer.parseInt(twoDay[2]),Integer.parseInt(twoDay[0])-1,Integer.parseInt(twoDay[1]));
 
-        return Today.after(Start) && Today.before(End);
+        return Today.after(Start) && Today.before(End) || Today.equals(Start);
     }
 }
